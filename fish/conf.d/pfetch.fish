b27b64a60c7af17533b0f0a36fa4928b8e4236f4
hihity <hihityyy@proton.me>
2026-08-09 10:12:05 +0300

    DOTFILES and README: big update

---
+++ fish/conf.d/pfetch.fish
@@ -0,0 +1,1 @@
+function pfetch --wraps pfetch; env PF_INFO="ascii title os wm shell uptime" PF_SEP=" · " PF_ALIGN=12 PF_GAP=1 pfetch; end
--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,1 @@
-source $__fish_config_dir/include/alias.fish
-source $__fish_config_dir/include/environment.fish
-source $__fish_config_dir/include/function.fish
 zoxide init fish | source
